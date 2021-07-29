e1cee38b9f2b7decb8cec0f1b76393ad34a659eb
ketakideshmukh <ketaki99deshmukh@gmail.com>
2021-07-29 10:01:38 -0800

    Add project files.

---
+++ jenkinsproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jenkinsproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
